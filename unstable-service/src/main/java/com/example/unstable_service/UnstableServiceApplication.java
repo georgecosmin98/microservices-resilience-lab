bd4b5598e58d08dccd9e8d1bc7cc03cb48402fc4
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2025-11-24 10:50:47 +0200

    Initialize Unstable Service and Client Service Microservices

---
+++ unstable-service/src/main/java/com/example/unstable_service/UnstableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.unstable_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnstableServiceApplication.class, args);
+	}
+
+}
---
+++ unstable-service/src/test/java/com/example/unstable_service/UnstableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unstable_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnstableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
